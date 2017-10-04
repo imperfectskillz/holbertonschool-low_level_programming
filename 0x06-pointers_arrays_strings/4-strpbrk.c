e2c34fee3e9816103e88f06352a3474a445b5ef0
imperfectskillz <j.choi.89@gmail.com>
2017-10-04 05:09:52 +0000

    completed ex. 4

--- 0x06-pointers_arrays_strings/4-strpbrk.c
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -9,3 +9,17 @@
 
 char *_strpbrk(char *s, char *accept)
 {
+	unsigned int i;
+
+	for ( ; *s != '\0'; s++)
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+	}
+	return (NULL);
+}
