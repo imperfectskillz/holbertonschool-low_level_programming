fe8d47b88de49b8c1770de28ffa47be464778a5f
imperfectskillz <j.choi.89@gmail.com>
2017-10-04 05:15:34 +0000

    finished ex. 5

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -9,4 +9,21 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	
+	while (*haystack)
+	{
+		char *hay = haystack;
+		char *need = needle;
+
+		while (*haystack && *need && *haystack == *need)
+		{
+			haystack++;
+			need++;
+		}
+		if (!*need)
+		{
+			return (hay);
+		}
+		haystack = hay + 1;
+	}
+	return (NULL);
+}
