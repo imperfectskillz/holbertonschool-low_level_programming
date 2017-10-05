9b434038ebdf5d1df618d0538e51e07e664675e3
imperfectskillz <j.choi.89@gmail.com>
2017-10-04 23:23:54 -0700

    Create 2-strlen_recursion.c

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *_strlen_recursion - returns length
+ *@s: to be tested
+ *REturn: length
+ */
+
+int _strlen_recursion(char *s)
+{
+        int i = 0;
+
+        if (s[i] != '\0')
+        {
+                _strlen_recursion(++i);
+        }
+        else
+        {
+                return;
+        }
+        return (i);
+}
