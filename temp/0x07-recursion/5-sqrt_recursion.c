dfc1678915a3f24ee31208b3dd5777a6df3a11c7
imperfectskillz <j.choi.89@gmail.com>
2017-10-05 17:00:43 +0000

    repo

---
+++ temp/0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ *_sqrt_recursion - returns the natural square root of a number
+ *@n: number
+ *Return: number
+ */
+
+int _sqrt_recursion(int n)
+{
+	
---
+++ temp/0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ *is_prime_number - determines prime number
+ *@n: number
+ *Returns: 1 or 0
+ */
+
+int is_prime_number(int n)
+{
+	
