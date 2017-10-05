da20c7baa49a0ef08f856555194ee80dd24c9145
imperfectskillz <j.choi.89@gmail.com>
2017-10-04 23:24:27 -0700

    Create 3-factorial.c

---
+++ 0x07-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *factorial- returns factorial of given number
+ *@n: variable
+ *REturn: factorial
+ */
+
+int factorial(int n)
+{
+        if (n < 0)
+        {
+                return (-1);
+        }
+        if (n == 0)
+        {
+                return (1);
+        }
+        else
+        {
+                return (n * factorial(n - 1));
+        }
+}
