fde9db298698a789a2b0b81be62088443e98f5a4
imperfectskillz <j.choi.89@gmail.com>
2017-10-06 20:09:27 +0000

    updated info

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -3,17 +3,17 @@
 /**
  *_strlen_recursion - returns length
  *@s: to be tested
- *REturn: length
+ *Return: length
  */
 
 int _strlen_recursion(char *s)
 {
-       if (*s == '\0')
-       {
-               return (0);
-       }        
-       else
-       {
-               return (_strlen_recursion(s + 1) + 1);
-       }
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (_strlen_recursion(s + 1) + 1);
+	}
 }
