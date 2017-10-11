b81f49c49fb55253c25a88c01913a74d86a9122a
imperfectskillz <j.choi.89@gmail.com>
2017-10-11 05:23:59 +0000

    completed 1

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+int _strlen(char *s);
+
+/**
+ *_strdup - returns a pointer to newly allocated space
+ *@str: given string
+ *Return: 0 or 1
+ */
+
+char *_strdup(char *str)
+{
+	char *result;
+	int i;
+	int length;
+
+	if (str == NULL)
+		return (NULL);
+
+	length = _strlen(str);
+	result = (char *)malloc(sizeof(char) * size + 1);
+
+	if (result == NULL)
+		return (NULL);
+	while (i < length)
+		result[i] = str[i], i++;
+	return (result);
+}
+
+/**
+ * _strlen - returns length
+ *@s: string
+ *
+ *Return: length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; s++)
+	{
+		i++;
+	}
+	return (i);
+}
