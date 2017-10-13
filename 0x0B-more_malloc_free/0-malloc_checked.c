fd70e2443a0ef318c0734e6a866e1682915b82e3
imperfectskillz <j.choi.89@gmail.com>
2017-10-13 04:28:27 +0000

    hi

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  *malloc_checked - allocates memory
@@ -12,9 +13,8 @@ void *malloc_checked(unsigned int b)
 	void *p;
 
 	p = malloc(b);
+
 	if (p == NULL)
-	{
 		exit(98);
-	}
 	return (p);
 }
