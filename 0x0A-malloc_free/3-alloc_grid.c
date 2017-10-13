f03ab47a98404f14d0c37a973df5d1c21c158a9c
imperfectskillz <j.choi.89@gmail.com>
2017-10-13 06:02:50 +0000

    completed the rest

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -2,58 +2,36 @@
 #include "holberton.h"
 
 /**
- * argstostr - concatenates all arguments
- * @ac: arguments
- * @av: pointer to pointers
- *Return: pointer to new string
+ *alloc_grid - create 2d array of ints
+ *@width: width of array
+ *@height: height
+ *Return: pointer
  */
 
-char *argstostr(int ac, char **av)
+int **alloc_grid(int width, int height)
 {
+	int **grid;
 	int i, j;
-	int counter;
-	int length;
-	char *result;
 
-	if (ac == 0 || av == NULL)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	length = ac + 1;
-	for (i = 0; i < ac; i++)
-	{
-		length += _strlen(av[i]);
-	}
-	result = malloc(sizeof(char) * length);
-	result[0] = '\0';
-	if (result == NULL)
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
 		return (NULL);
 
-	counter = 0;
-	for (i = 0; i < ac; i++)
+	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < _strlen(av[i]); j++, counter++)
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
 		{
-			result[counter] = av[i][j];
+			for (i--; i >= 0; i--)
+				free(grid[i]);
+			free{grid);
+			return (NULL);
 		}
-		result[counter] = '\n';
-		counter++;
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
 	}
-	result[length - 1] = '\0';
-	return (result);
-}
-
-/**
- * _strlen - return length of a string
- * @s: string to find the length of
- *
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; *s != '\0'; s++)
-		i++;
-
-	return (i);
+	return (grid);
 }
