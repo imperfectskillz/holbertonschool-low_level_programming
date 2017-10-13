f924db093917056626a77fb7c77bbcd8833bcb30
imperfectskillz <j.choi.89@gmail.com>
2017-10-13 05:14:23 +0000

    null byte

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -34,9 +34,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i;
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = '\0';
 	if (s2 == NULL)
-		s2 = "";
+		s2 = '\0';
 
 	length1 = _strlen(s1);
 	length2 = _strlen(s2);
