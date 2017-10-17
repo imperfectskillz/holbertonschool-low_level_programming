b68c236c36dadbcf3fc715051a9aee9d12f1c5ac
imperfectskillz <j.choi.89@gmail.com>
2017-10-17 01:43:31 +0000

    completed proj

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include <stdli.h>
+#include "dog.h"
+
+/**
+ *new_dog - creates a new dog
+ *@name: name
+ *@age: age
+ *@owner: owner
+ *Return: pointer to new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+	
