bc528dcfbd5c8c200def8e924078e9bbf9791d9d
imperfectskillz <j.choi.89@gmail.com>
2017-10-17 19:59:49 +0000

    completed 4

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,5 +1,8 @@
-#include <stdli.h>
+#include <stdlib.h>
 #include "dog.h"
+
+int _strlen(char *p);
+char *_strcpy(char *dest, char *src);
 
 /**
  *new_dog - creates a new dog
@@ -11,5 +14,59 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *p;
+	p = malloc(sizeof(char) * (_strlen(i) + 1));
+	if (p == NULL)
+		return (NULL);
+	p->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (p->name == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	p->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (p->owner == NULL)
+	{
+		free(p);
+		free(p->name);
+		return;
+	}
+	_strcpy(p->name, name);
+	_strcpy(p->owner, owner);
+	p->age = age;
+	return (p);
+}
 
-	
+/**
+ *_strlen - gets length of string
+ *@p: pointer
+ *Return: length
+ */
+
+int _strlen(char *p)
+{
+	int i = 0;
+	while (p[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ *_strcpy- copies string
+ *@dest: destination
+ *@src: source
+ *Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest = dest + '\0';
+	return (dest);
+}
