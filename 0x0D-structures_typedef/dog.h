45d253df4f3770bc4a8f470513dd8ec084ddd606
imperfectskillz <j.choi.89@gmail.com>
2017-10-17 01:12:08 +0000

    created dog.h

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog
+ *@name: char *
+ *@age: float
+ *@owner: char *
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
