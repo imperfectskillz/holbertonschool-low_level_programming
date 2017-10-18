cdd9943044fcfc101479ee292da01463140f0896
imperfectskillz <j.choi.89@gmail.com>
2017-10-18 20:15:30 +0000

    completed first ex

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - calls function that prints name
+ *@name: string to be called on
+ *@f: pointer function
+ *Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	void (*f)(char *) = &name;
+}
