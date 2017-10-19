6dd6c4b8c57c5ff934b377654a3e3e8c293e5681
imperfectskillz <j.choi.89@gmail.com>
2017-10-19 04:39:46 +0000

    fixed header file

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -1,10 +1,8 @@
-#include "function_pointers.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include "function_pointers.h"
 
 /**
  *array_iterator - executes a function given as a
- * parameter on each element of an array
  *@array: given
  *@size: size of array
  *@action: function
@@ -16,7 +14,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int i;
 
 	if (array == NULL || action == NULL)
+	{
 		return;
+	}
 	for (i = 0; i < size; i++)
+	{
 		action(array[i]);
+	}
 }
