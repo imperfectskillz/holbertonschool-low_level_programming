149d65481ae57457f985bf7308be7aee54a8b426
imperfectskillz <j.choi.89@gmail.com>
2017-10-19 05:29:28 +0000

    completed part of 3

---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "function_pointers.h"
+
+/**
+ *op_add - adds a and b
+ *@a: first int
+ *@b: second int
+ *Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub = subtracts a and b
+ *@a: first int
+ *@b: second int
+ *Return: sub
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - multiplies a and b
+ *@a: first int
+ *@b: second int
+ *Return: mult
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - divides
+ *@a: var
+ *@b: var
+ *Return: div
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - modulo
+ *@a: var
+ *@b: var
+ *Return: mod
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
