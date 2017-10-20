42bf5e2fd2f6ab07dc3f9a6e2fd3f20b1def0eea
imperfectskillz <j.choi.89@gmail.com>
2017-10-20 21:11:56 +0000

    fixed n - 1

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -13,16 +13,19 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int i;
+	char *c;
 
 	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
 	{
-		if (va_arg(valist, char *) == NULL)
+		c = va_arg(valist, char *);
+
+		if (c == NULL)
 			printf("(nil)");
 		else
-			printf("%s", va_arg(valist, char *));
-		if (separator != NULL && i != n)
+			printf("%s", c);
+		if (separator != NULL && i != n - 1)
 			printf("%s", separator);
 	}
 	printf("\n");
