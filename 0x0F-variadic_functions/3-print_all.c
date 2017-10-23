55ba0a66685539a78d2fed66776deede667f735f
imperfectskillz <j.choi.89@gmail.com>
2017-10-23 06:13:40 +0000

    half completed 3

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ *print_all - prints all
+ *@format: formatter
+ *Return: 0
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list valist;
+
+	va_start(valist, format);
+
+	int i = 0;
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(valist, char));
+			break;
+		case 'i':
+			printf("%d", va_arg(valist, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, float));
+			break;
+		case 's':
+			printf("%s", va_arg(valist, char*));
+			break;
+		}
+	}
+	printf("\n");
+}
