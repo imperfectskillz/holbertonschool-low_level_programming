80e1b8c7eed902471a85afde494ad51418dc716b
imperfectskillz <j.choi.89@gmail.com>
2017-10-26 21:12:02 +0000

    check if h is NULL, check if h->str is NULL

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -16,15 +16,14 @@ size_t print_list(const list_t *h)
 	if (h == NULL)
 		return (0);
 
-	while (h->next != NULL)
+	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] %s\n", "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		nodeCounter++;
-	}
-	if (h->next == NULL)
-	{
-		printf("[%d] %s\n", h->len, h->str);
 	}
 	return (nodeCounter);
 }
