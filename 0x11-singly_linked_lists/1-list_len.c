e03a5d282ea24455b1d5b3b1c06998dac418a590
imperfectskillz <j.choi.89@gmail.com>
2017-10-26 21:18:25 +0000

    completed #1

--- 0x11-singly_linked_lists/1-list_len.c
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -1,1 +1,22 @@
 #include "lists.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ *list_len - returns number of elements
+ *@h: pointer to head
+ *Return: elements number
+ */
+
+size_t list_len(const list_t *h)
+{
+	int counter = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		++counter;
+	}
+	return (counter);
+}
