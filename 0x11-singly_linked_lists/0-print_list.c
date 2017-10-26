f27887c8bf70573eb645dea7e36a29403af7ee49
imperfectskillz <j.choi.89@gmail.com>
2017-10-26 21:13:27 +0000

    incrementator to 0

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@
 
 size_t print_list(const list_t *h)
 {
-	int nodeCounter = 1;
+	int nodeCounter = 0;
 
 	if (h == NULL)
 		return (0);
@@ -23,7 +23,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nodeCounter++;
+		++nodeCounter;
 	}
 	return (nodeCounter);
 }
