3eeb5891493e14e7e57d7a611aba59b70ca8deea
imperfectskillz <j.choi.89@gmail.com>
2017-11-02 06:58:18 +0000

    added iterator to loop through nodes

---
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ *insert-nodeint_at_index - inserts a new node at given pos.
+ *@head: pointer pointer head
+ *@idx: given point
+ *@n: data
+ *Return: addy of new node
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp;
+	listint_t *new;
+	int i = 0;
+
+	new = malloc(sizeof(listint_t));
+
+	if (head == NULL || new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	while (temp != NULL)
+	{
+		if (i == idx)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		i++;
+		temp = temp->next;
+	}
+	return (new);
+}
