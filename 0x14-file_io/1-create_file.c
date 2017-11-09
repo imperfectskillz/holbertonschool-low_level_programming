80678fbcc78fc638ae9b28cd77b659e33d58c84c
imperfectskillz <j.choi.89@gmail.com>
2017-11-09 07:51:48 +0000

    used O_APPEND for appending

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ *create_file - creates name of the file
+ *@filename: name of file
+ *@text_content: text to be inserted
+ *Return: 1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int create;
+	int fwrite;
+	int length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	create = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (create == -1)
+		return (-1);
+
+	while (create[length] != '\0')
+		length++;
+
+	fwrite = write(create, text_content, length);
+
+	close(create);
+	return (1);
+}
---
+++ 0x14-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ *append_text_to_file - appends text
+ *@filename: name of file
+ *@text_content: text
+ *Return: 1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int create;
+	int fwrite;
+	int length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	create = open(filename, O_WRONLY | O_APPEND);
+
+	if (create == -1)
+		return (-1);
+
+	while (create[length] != '\0')
+		length++;
+
+	fwrite = write(create, text_content, length);
+
+	close(create);
+	return (1);
+}
+
