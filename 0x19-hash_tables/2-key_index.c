f17c7d4050242678889a9640d830e0f5570dd827
imperfectskillz <j.choi.89@gmail.com>
2018-01-18 21:56:16 +0000

    modified

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -11,6 +11,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int key_idx = 0;
 
-	key_index = (hash_djb2(key) % size);
+	key_index = hash_djb2(key);
+	key_idx = key_idx % size;
 	return (key_index);
 }
