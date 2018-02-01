4f0421756140d1c8c11d5efd9286f16fd9ba858f
imperfectskillz <j.choi.89@gmail.com>
2018-02-01 05:57:55 +0000

    changed flag statement

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -11,19 +11,19 @@ void bubble_sort(int *array, size_t size)
 {
 	int temp;
 	int i;
-	int flag = 0;
+	int flag = 1;
 
 	if (array == NULL)
 		return;
 
-	while (flag != 1)
+	while (flag != 0)
 	{
-		flag = 1;
+		flag = 0;
 		for (i = 0; i < size - 1; i++)
 		{
 			if (array[i] > array[i+1])
 			{
-				flag = 0;
+				flag = 1;
 				temp = array[i];
 				array[i] = array[i+1];
 				array[i+1] = temp;
