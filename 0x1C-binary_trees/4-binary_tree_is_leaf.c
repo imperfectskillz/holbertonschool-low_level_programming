6ac673703d0d435b4a516895f8ece8e7060c05c0
imperfectskillz <j.choi.89@gmail.com>
2018-03-07 23:04:00 +0000

    leaf check

---
+++ 0x1C-binary_trees/4-binary_tree_is_leaf.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_is_leaf - checks if leaf or not
+ *@node: node to be checked
+ *Return: true or false
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return 0;
+
+	if (node->left == NULL && node->right == NULL)
+		return 1;
+	return 0;
+}
