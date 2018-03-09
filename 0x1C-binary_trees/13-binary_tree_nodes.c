cb4d4ee03dbadf990937741883372aba9d117c83
imperfectskillz <j.choi.89@gmail.com>
2018-03-09 03:42:33 +0000

    13

---
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_nodes - counts nodes
+ *@tree: tree
+ *Return: count
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (tree == NULL)
+		return 0;
+
+	if (tree->left == NULL && tree->right == NULL)
+		return 0;
+
+	count += 1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+
+	return count;
+}
