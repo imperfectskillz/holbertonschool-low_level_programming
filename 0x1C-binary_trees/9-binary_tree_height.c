a5157c244dc191c7c986c8611e572faedda389b7
imperfectskillz <j.choi.89@gmail.com>
2018-03-09 00:18:28 +0000

    9

---
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_height - returns height
+ *@tree: tree
+ *Return: height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left;
+	size_t right;
+
+	if (tree == NULL)
+		return 0;
+
+	if (tree->left == NULL && tree->right == NULL)
+		return 0;
+
+	left = binary_tree_height(tree->left) + 1;
+	right = binary_tree_height(tree->right) + 1;
+
+	if (left > right)
+		return left;
+	return right;
+}
