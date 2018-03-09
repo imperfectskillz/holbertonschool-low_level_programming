ce4046508d64cf31f8017a48c403981e494948ea
imperfectskillz <j.choi.89@gmail.com>
2018-03-09 21:55:54 +0000

    null check

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -12,6 +12,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *temp;
 
 	temp = malloc(sizeof(binary_tree_t));
+	if (temp == NULL)
+		return (NULL);
 
 	temp->n = value;
 	temp->parent = parent;
